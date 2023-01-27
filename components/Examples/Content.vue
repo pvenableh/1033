b4bdfce30133635d8b525a3b3460a370584e2570
pvenableh <peter@huestudios.com>
2023-01-27 16:18:38 -0500

    Initial push.

---
+++ components/Examples/Content.vue
@@ -0,0 +1,70 @@
+<template>
+  <ExamplesTwoCols label="Dynamic Content">
+    <template #content>
+      <p>
+        This example shows how to fetch content from the Directus API and render
+        that using dynamic routes in Nuxt.
+      </p>
+      <p>
+        Look for the
+        <code>[slug].vue</code> component within the
+        <code>/pages</code> directory.
+      </p>
+    </template>
+    <template #default>
+      <div v-if="loading" class="flex items-center justify-center flex-1">
+        <VLoading class="w-24 h-24 text-primary-600" />
+      </div>
+      <div
+        v-else-if="units.length === 0"
+        class="relative block w-full px-12 py-16 text-center border-2 border-gray-300 border-dashed rounded-lg dark:border-gray-700 hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500"
+      >
+        <p class="block mt-2 font-medium text-gray-900 dark:text-gray-200">
+          No units loaded. <br />What are you waiting for?
+        </p>
+        <VButton class="mt-2" @click="fetchUnits()" variant="primary">
+          Fetch Units</VButton
+        >
+      </div>
+      <ul class="space-y-6" v-else-if="units.length > 0">
+        <li v-for="unit in units" :key="unit.id">
+          <ExamplesUnitCard
+            :unit="unit"
+          />
+        </li>
+      </ul>
+      <VButton
+        v-if="units.length > 0"
+        class="mt-4"
+        @click="units = []"
+        variant="default"
+      >
+        Clear Units</VButton
+      >
+    </template>
+  </ExamplesTwoCols>
+</template>
+
+<script setup>
+const { $directus } = useNuxtApp()
+const { fileUrl } = useFiles()
+
+const units = ref([])
+const loading = ref(false)
+
+async function fetchUnits() {
+  loading.value = true
+  try {
+    const { data } = await $directus.items('units').readByQuery({
+      // filter: {
+      //   status: { _eq: 'published' },
+      // }
+    })
+    units.value = data
+  } catch (e) {
+    console.error(e)
+  } finally {
+    loading.value = false
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,216 @@
+<template>
+  <div class="relative w-full bg-center bg-cover bg-no-repeat min-h-screen flex items-center justify-center flex-col px-4 md:px-12 home" style="background-image: url(https://admin.1033lenox.com/assets/bda84bff-e6e1-4bfb-ac82-f44a999d7073?key=xlarge)">
+   <Logo color="#ffffff"/>
+   <h1 class="mt-10">a new digital experience coming soon.</h1>
+  </div>
+</template>
+
+<script setup>
+const title = '1033 Lenox Ave'
+useHead({
+  title: title,
+})
+</script>
+<style >
+.home {
+    background-color: rgba(0,0,0,0.35);
+    background-blend-mode: darken;
+
+h1 {
+    font-size: 12px;
+    background: linear-gradient(-45deg, #F5FF00, white);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    bottom: calc(25% - 5px);
+    @apply uppercase tracking-wider absolute;
+}
+.logo {
+    max-width: 650px;
+    filter: drop-shadow(0 0.2rem 0.25rem rgba(0, 0, 0, 0.5));
+  
+  path {
+    opacity: 0.4;
+    animation-name: example;
+    animation-duration: 5s;
+    animation-timing-function: var(--curve);
+    animation-iteration-count: infinite;
+  }
+  path:nth-of-type(1) {
+    animation-delay: 0.1s;
+  }
+  path:nth-of-type(2) {
+    animation-delay: 0.2s;
+  }
+  path:nth-of-type(3) {
+    animation-delay: 0.3s;
+  }
+  path:nth-of-type(4) {
+    animation-delay: 0.4s;
+  }
+  path:nth-of-type(5) {
+    animation-delay: 0.5s;
+  }
+  path:nth-of-type(6) {
+    animation-delay: 0.6s;
+  }
+  path:nth-of-type(7) {
+    animation-delay: 0.7s;
+  }
+  path:nth-of-type(8) {
+    animation-delay: 0.8s;
+  }
+  path:nth-of-type(9) {
+    animation-delay: 0.9s;
+  }
+  path:nth-of-type(10) {
+    animation-delay: 1s;
+  }
+  path:nth-of-type(11) {
+    animation-delay: 1.1s;
+  }
+  path:nth-of-type(12) {
+    animation-delay: 1.2s;
+  }
+  path:nth-of-type(13) {
+    animation-delay: 1.3s;
+  }
+  path:nth-of-type(14) {
+    animation-delay: 1.4s;
+  }
+  path:nth-of-type(15) {
+    animation-delay: 1.5s;
+  }
+  path:nth-of-type(16) {
+    animation-delay: 1.6s;
+  }
+  path:nth-of-type(17) {
+    animation-delay: 1.7s;
+  }
+  path:nth-of-type(18) {
+    animation-delay: 1.8s;
+  }
+  path:nth-of-type(19) {
+    animation-delay: 1.9s;
+  }
+  path:nth-of-type(20) {
+    animation-delay: 2.0s;
+  }
+  path:nth-of-type(21) {
+    animation-delay: 2.1s;
+  }
+  path:nth-of-type(22) {
+    animation-delay: 2.2s;
+  }
+  path:nth-of-type(23) {
+    animation-delay: 2.3s;
+  }
+  path:nth-of-type(24) {
+    animation-delay: 2.4s;
+  }
+  path:nth-of-type(25) {
+    animation-delay: 2.5s;
+  }
+  path:nth-of-type(26) {
+    animation-delay: 0.1s;
+  }
+  path:nth-of-type(27) {
+    animation-delay: 0.2s;
+  }
+  path:nth-of-type(28) {
+    animation-delay: 0.3s;
+  }
+  path:nth-of-type(29) {
+    animation-delay: 0.4s;
+  }
+  path:nth-of-type(30) {
+    animation-delay: 0.5s;
+  }
+  path:nth-of-type(31) {
+    animation-delay: 0.6s;
+  }
+  path:nth-of-type(32) {
+    animation-delay: 0.7s;
+  }
+  path:nth-of-type(33) {
+    animation-delay: 0.8s;
+  }
+  path:nth-of-type(34) {
+    animation-delay: 0.9s;
+  }
+  path:nth-of-type(35) {
+    animation-delay: 1s;
+  }
+  path:nth-of-type(36) {
+    animation-delay: 1.1s;
+  }
+  path:nth-of-type(37) {
+    animation-delay: 1.2s;
+  }
+  path:nth-of-type(38) {
+    animation-delay: 1.3s;
+  }
+  path:nth-of-type(39) {
+    animation-delay: 1.4s;
+  }
+  path:nth-of-type(40) {
+    animation-delay: 1.5s;
+  }
+  path:nth-of-type(41) {
+    animation-delay: 1.6s;
+  }
+  path:nth-of-type(42) {
+    animation-delay: 1.7s;
+  }
+  path:nth-of-type(43) {
+    animation-delay: 1.8s;
+  }
+  path:nth-of-type(44) {
+    animation-delay: 1.9s;
+  }
+  path:nth-of-type(45) {
+    animation-delay: 2.0s;
+  }
+  path:nth-of-type(46) {
+    animation-delay: 2.1s;
+  }
+  path:nth-of-type(47) {
+    animation-delay: 2.2s;
+  }
+  path:nth-of-type(48) {
+    animation-delay: 2.23s;
+  }
+  path:nth-of-type(49) {
+    animation-delay: 2.26s;
+  }
+  path:nth-of-type(50) {
+    animation-delay: 2.29s;
+  }
+  path:nth-of-type(51) {
+    animation-delay: 2.32s;
+  }
+  path:nth-of-type(52) {
+    animation-delay: 2.35s;
+  }
+  path:nth-of-type(53) {
+    animation-delay: 2.38s;
+  }
+  path:nth-of-type(54) {
+    animation-delay: 2.41s;
+  }
+  path:nth-of-type(55) {
+    animation-delay: 2.44s;
+  }
+  path:nth-of-type(56) {
+    animation-delay: 2.47s;
+  }
+  path:nth-of-type(57) {
+    animation-delay: 2.5s;
+  }
+}
+@keyframes example {
+  0% {opacity: 0.4}
+  50% {opacity:1}
+  100% {opacity: 0.4}
+}
+}
+</style>
