1d7b021ea690734a9bdbec1f19ab2f5186a9570e
pvenableh <peter@huestudios.com>
2023-03-01 20:11:05 -0500

    Update announcement.js

--- server/api/email/announcement.js
+++ server/api/email/announcement.js
@@ -1,46 +1,68 @@
 import sgMail from '@sendgrid/mail';
 
 sgMail.setApiKey('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');
+const array1 = [
+    {
+        first_name: "Peter",
+        unit: "314",
+        email: "peter@huestudios.com"
+    },
+    {
+        first_name: "Oscar",
+        unit: "214",
+        email: "petervhoffman@gmail.com"
+    },
+    {
+        first_name: "Test",
+        unit: "201",
+        email: "huestudios.com@gmail.com"
+    }
+]
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
-    const message = {
-        personalizations: [{
-            to: [{
-                email: 'peter@huestudios.com',
+    const messages = []
+    array1.forEach(element => {
+        messages.push(
+            {
+            personalizations: [{
+                to: [{
+                    email: element.email,
+                }],
+                bcc: [{
+                    email: 'huestudios.com@gmail.com',
+                }]
             }],
-            bcc: [{
-                email: 'huestudios.com@gmail.com',
-            }]
-        }],
-        from: {
-            email: 'mail@1033lenox.com',
-            name: '1033 Lenox'
-        },
-        template_id: 'd-035e7712976d45aaa5143d8a1042aee7',
-        replyTo: {
-            email: 'mail@1033lenox.com',
-            name: '1033 Lenox'
-        },
-        subject: 'Attention Peter: ',
-        content: [{
-            type: 'text/html',
-            value: '&nbsp;'
-        }],
-        dynamicTemplateData: {
-            first_name: "Peter",
-            unit: "314",
-            title: "Testing This",
-            subtitle: body.payload.subtitle,
-            urgent: true,
-            content: body.payload.content,
-        },
-        categories: [
-            'announcements'
-        ],
+            from: {
+                email: 'mail@1033lenox.com',
+                name: '1033 Lenox'
+            },
+            template_id: 'd-035e7712976d45aaa5143d8a1042aee7',
+            replyTo: {
+                email: 'mail@1033lenox.com',
+                name: '1033 Lenox'
+            },
+            subject: 'Attention ' + element.first_name + ': ' + body.payload.title,
+            content: [{
+                type: 'text/html',
+                value: '&nbsp;'
+            }],
+            dynamicTemplateData: {
+                first_name: element.first_name,
+                unit: element.unit,
+                title: body.payload.title,
+                subtitle: body.payload.subtitle,
+                urgent: body.payload.urgent,
+                content: body.payload.content,
+            },
+            categories: [
+                'announcements'
+            ],
+        }
+        )
     }
+ 
 
-sgMail.send(message).then(() => {}, error => {
-    
+sgMail.send(messages).then(() => {}, error => {
     console.error(error)
     if (error.response) {
       console.error(error.response.body)
