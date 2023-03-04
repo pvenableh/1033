d35d7192e7bac6fc5a4ee6969c915005dc0fb0bc
pvenableh <peter@huestudios.com>
2023-03-04 10:30:19 -0500

    Update announcement.js

--- server/api/email/announcement.js
+++ server/api/email/announcement.js
@@ -4,7 +4,7 @@ sgMail.setApiKey('SG.33tfJzB6TcuhxlAqZF8f9g.MpOZtqAptJWkJPalpHKFG7qg5CbDgz8lWgoK
 const array1 = [
     {
         first_name: "Peter",
-        unit: "312",
+        unit: "314",
         email: "peter@huestudios.com"
     },
     {
@@ -15,13 +15,11 @@ const array1 = [
     {
         first_name: "Test",
         unit: "201",
-        email: "contact@huestudios.com"
+        email: "huestudios.com@gmail.com"
     }
 ]
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
-    // const announcement = await $fetch(`https://admin.1033lenox.com/items/announcements/${body.payload.id}`)
-   
     const messages = []
     array1.forEach(element => {
         messages.push(
@@ -55,7 +53,6 @@ export default defineEventHandler(async (event) => {
                     subtitle: body.payload.subtitle,
                     urgent: body.payload.urgent,
                     content: body.payload.content,
-                    // content: announcement,
                 },
                 categories: [
                     'announcements'
@@ -68,10 +65,9 @@ export default defineEventHandler(async (event) => {
     sgMail.send(messages).then(() => {}, error => {
         console.error(error)
         if (error.response) {
-        console.error(error.response.body)
-        const error = error.response.body
-
+            console.error(error.response.body)
+            const error = error.response.body
         }
-    })
-  return body.payload
+  })
+  return body
 })
