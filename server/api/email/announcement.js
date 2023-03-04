7d8ed19d615c2b5d7ed2bbde5d69aa0cbd7a2388
pvenableh <peter@huestudios.com>
2023-03-04 09:59:39 -0500

    Testing announcement.

--- server/api/email/announcement.js
+++ server/api/email/announcement.js
@@ -4,7 +4,7 @@ sgMail.setApiKey('SG.33tfJzB6TcuhxlAqZF8f9g.MpOZtqAptJWkJPalpHKFG7qg5CbDgz8lWgoK
 const array1 = [
     {
         first_name: "Peter",
-        unit: "314",
+        unit: "312",
         email: "peter@huestudios.com"
     },
     {
@@ -20,55 +20,57 @@ const array1 = [
 ]
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
+    const announcement = await $fetch(`https://admin.1033lenox.com/items/announcements/${body.payload.id}`)
+    return announcement
     const messages = []
-    array1.forEach(element => {
-        messages.push(
-            {
-                personalizations: [{
-                    to: [{
-                        email: element.email,
-                    }],
-                    bcc: [{
-                        email: 'huestudios.com@gmail.com',
-                    }]
-                }],
-                from: {
-                    email: 'mail@1033lenox.com',
-                    name: '1033 Lenox'
-                },
-                template_id: 'd-035e7712976d45aaa5143d8a1042aee7',
-                replyTo: {
-                    email: 'mail@1033lenox.com',
-                    name: '1033 Lenox'
-                },
-                subject: 'Attention ' + element.first_name + ': ' + body.payload.title,
-                content: [{
-                    type: 'text/html',
-                    value: '&nbsp;'
-                }],
-                dynamicTemplateData: {
-                    first_name: element.first_name,
-                    unit: element.unit,
-                    title: body.payload.title,
-                    subtitle: body.payload.subtitle,
-                    urgent: body.payload.urgent,
-                    content: body.payload.content,
-                },
-                categories: [
-                    'announcements'
-                ],
-            }
-        )
-    })
+    // array1.forEach(element => {
+    //     messages.push(
+    //         {
+    //             personalizations: [{
+    //                 to: [{
+    //                     email: element.email,
+    //                 }],
+    //                 bcc: [{
+    //                     email: 'huestudios.com@gmail.com',
+    //                 }]
+    //             }],
+    //             from: {
+    //                 email: 'mail@1033lenox.com',
+    //                 name: '1033 Lenox'
+    //             },
+    //             template_id: 'd-035e7712976d45aaa5143d8a1042aee7',
+    //             replyTo: {
+    //                 email: 'mail@1033lenox.com',
+    //                 name: '1033 Lenox'
+    //             },
+    //             subject: 'Attention ' + element.first_name + ': ' + body.payload.title,
+    //             content: [{
+    //                 type: 'text/html',
+    //                 value: '&nbsp;'
+    //             }],
+    //             dynamicTemplateData: {
+    //                 first_name: element.first_name,
+    //                 unit: element.unit,
+    //                 title: body.payload.title,
+    //                 subtitle: body.payload.subtitle,
+    //                 urgent: body.payload.urgent,
+    //                 content: body.payload.content,
+    //             },
+    //             categories: [
+    //                 'announcements'
+    //             ],
+    //         }
+    //     )
+    // })
  
 
-    sgMail.send(messages).then(() => {}, error => {
-        console.error(error)
-        if (error.response) {
-        console.error(error.response.body)
-        const error = error.response.body
+    // sgMail.send(messages).then(() => {}, error => {
+    //     console.error(error)
+    //     if (error.response) {
+    //     console.error(error.response.body)
+    //     const error = error.response.body
 
-        }
-    })
-  return body
+    //     }
+    // })
+//   return body
 })
