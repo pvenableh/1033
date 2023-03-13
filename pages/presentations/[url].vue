da8c18eeffdc4537c13ee9582d712d54d32f0c7d
pvenableh <peter@huestudios.com>
2023-03-13 16:07:58 -0400

    Presentation pages added.

---
+++ pages/presentations/[url].vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="relative w-full presentation">
+    <swiper
+      :style="{
+        '--swiper-navigation-color': '#fff',
+        '--swiper-pagination-color': '#fff',
+      }"
+      :speed="600"
+      :parallax="true"
+      :pagination="{
+        clickable: true,
+      }"
+      :navigation="true"
+      :modules="modules"
+      @swiper="onSwiper"
+      @slideChange="onSlideChange"
+      class="min-h-screen mySwiper"
+    >
+      <swiper-slide
+        v-for="(slide, index) in presentation.slides"
+        :key="index"
+        class="min-h-screen flex items-center justify-center bg-zinc-300"
+      >
+        <div class="min-h-screen flex items-center justify-center">
+          <img
+            :src="
+              'https://admin.1033lenox.com/assets/' +
+              slide.directus_files_id.id +
+              '?key=large'
+            "
+            :alt="presentation.title + ' Presentation'"
+            class="w-full h-auto shadow-lg"
+          />
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script setup>
+const { params, path } = useRoute()
+const { getItems } = useDirectusItems()
+const presentationReq = await getItems({
+  collection: 'presentations',
+  params: {
+    filter: {
+      url: {
+        _eq: params.url,
+      },
+    },
+    fields: ['id,title,description,url,slides.directus_files_id.id'],
+  },
+})
+
+const presentation = ref(presentationReq[0])
+import { Swiper, SwiperSlide } from 'swiper/vue'
+
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import { Navigation, Pagination, Parallax } from 'swiper'
+const modules = [Parallax, Pagination, Navigation]
+</script>
+<style>
+.presentation {
+  .swiper-button-prev,
+  .swiper-button-next {
+    color: #eeeeee;
+  }
+}
+</style>
---
+++ pages/presentations/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="relative w-full min-h-screen flex items-center justify-center flex-col px-4 md:px-12 presentations"
+  >
+    <div class="w-full flex items-center justify-center flex-row flex-wrap">
+      <nuxt-link
+        v-for="(item, index) in presentations"
+        :key="index"
+        :href="'/presentations/' + item.url"
+      >
+        {{ item.title }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { getItems } = useDirectusItems()
+const presentations = await getItems({
+  collection: 'presentations',
+  params: {
+    fields: ['title,description,url,slides.directus_files_id.id'],
+  },
+})
+</script>
+<style >
+</style>
