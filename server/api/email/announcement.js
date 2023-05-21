5671f3a5ced72bed67601347e62be3da127da2f7
pvenableh <peter@huestudios.com>
2023-05-21 16:57:40 -0400

    Update announcement.js

--- server/api/email/announcement.js
+++ server/api/email/announcement.js
@@ -1,72 +1,52 @@
 import sgMail from '@sendgrid/mail';
 
 sgMail.setApiKey('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');
-const array1 = [
-    {
-        first_name: "Peter",
-        unit: "314",
-        email: "peter@huestudios.com"
-    },
-    {
-        first_name: "Oscar",
-        unit: "214",
-        email: "petervhoffman@gmail.com"
-    },
-    {
-        first_name: "Test",
-        unit: "201",
-        email: "contact@huestudios.com"
-    }
-]
+
 export default defineEventHandler(async (event) => {
     const body = await readBody(event)
     const recipients = body.data.recipients
     const messages = []
     recipients.forEach(element => {
-        // let units
-        // if (element.people_id.unit.length > 1) {
-        //     units = 
-        // } else {
-        //     units = element.people_id.unit
-        // }
-        messages.push(
-            {
-                personalizations: [{
-                    to: [{
-                        email: element.people_id.email,
+        if (element.people_id.email && element.people_id.unit.length > 0) {
+            messages.push(
+                {
+                    personalizations: [{
+                        to: [{
+                            email: element.people_id.email,
+                        }],
+                        bcc: [{
+                            email: 'huestudios.com@gmail.com',
+                        }]
                     }],
-                    bcc: [{
-                        email: 'huestudios.com@gmail.com',
-                    }]
-                }],
-                from: {
-                    email: 'mail@1033lenox.com',
-                    name: '1033 Lenox'
-                },
-                template_id: 'd-035e7712976d45aaa5143d8a1042aee7',
-                replyTo: {
-                    email: 'mail@1033lenox.com',
-                    name: '1033 Lenox'
-                },
-                subject: 'Attention ' + element.people_id.first_name + ': ' + body.data.data.title,
-                content: [{
-                    type: 'text/html',
-                    value: '&nbsp;'
-                }],
-                dynamicTemplateData: {
-                    first_name: element.people_id.first_name,
-                    unit: element.people_id.unit[0].units_id.number,
-                    title: body.data.data.title,
-                    subtitle: body.data.data.subtitle,
-                    urgent: body.data.data.urgent,
-                    content: body.data.data.content,
-                    url: body.data.data.url,
-                },
-                categories: [
-                    'announcements'
-                ],
-            }
-        )
+                    from: {
+                        email: 'mail@1033lenox.com',
+                        name: '1033 Lenox'
+                    },
+                    template_id: 'd-035e7712976d45aaa5143d8a1042aee7',
+                    replyTo: {
+                        email: 'lenoxplazaboard@gmail.com',
+                        name: '1033 Lenox'
+                    },
+                    subject: 'Attention ' + element.people_id.first_name + ': ' + body.data.data.title,
+                    content: [{
+                        type: 'text/html',
+                        value: '&nbsp;'
+                    }],
+                    dynamicTemplateData: {
+                        first_name: element.people_id.first_name,
+                        unit: element.people_id.unit[0].units_id.number,
+                        title: body.data.data.title,
+                        subtitle: body.data.data.subtitle,
+                        urgent: body.data.data.urgent,
+                        content: body.data.data.content,
+                        url: body.data.data.url,
+                    },
+                    categories: [
+                        'announcements'
+                    ],
+                }
+            )
+        }
     })
  
 
