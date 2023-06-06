129acbf8ed55bd3292e6316404dced06f75a12e6
pvenableh <peter@huestudios.com>
2023-06-06 16:43:43 -0400

    Document update.

--- pages/documents/index.vue
+++ pages/documents/index.vue
@@ -1,8 +1,19 @@
 <template>
     <div
-        class="relative w-full min-h-screen flex items-center justify-center flex-col documents">
+        class="relative w-full min-h-screen flex items-center justify-center flex-col scroll-smooth documents">
         <h1 class="uppercase tracking-wide my-8">Lenox Plaza Association By-Laws</h1>
-        <div v-if="doc.document" v-html="doc.document" class="w-full by-laws"></div>
+        <div class="w-full flex flex-row items-start justify-center relative">
+            <div class="sticky flex flex-col items-start flex-shrink uppercase mr-10 text-right nav">
+                <h5 class="uppercase text-xs opacity-25 tracking-wider font-bold w-full mb-4">Sections</h5>
+                <a href="#one">Identity</a>
+                <a href="#two">Members</a>
+                <a href="#three">Members' Meeting</a>
+                <a href="#four">Directors</a>
+                <a href="#five">Officers</a>
+                <a href="#six">Fiscal Management</a>
+            </div>
+        <div v-if="doc.document" v-html="doc.document" class="pr-4 pl-4 lg:pl-10 w-full flex-grow scroll-smooth by-laws"></div>
+    </div>
     </div>
 </template>
 
@@ -18,15 +29,32 @@ const doc = await getItemById({
 
 </script>
 <style >
+ .nav {
+        width: 250px;
+        @apply pt-10 mt-10 top-0;
+        display: none;
+        @media (min-width: theme('screens.lg')) {
+            display: flex
+    }
+        a {
+            @apply w-full tracking-wider text-xs py-2;
+        }
+        h5 {
+            
+        }
+    }
 .by-laws {
     max-width: 800px;
-    @apply px-4;
+    
+
     @media (min-width: theme('screens.lg')) {
+        border-left: thin solid var(--grey);
     }
+   
     h1 {
         font-size: 24px;
         letter-spacing: 0.05em;
-        @apply mt-10 uppercase font-bold;
+        @apply pt-10 mt-10 uppercase font-bold;
     }
     h2 {
         font-size: 20px;
