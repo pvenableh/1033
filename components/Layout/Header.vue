a39a0c68f8cf2bb80c692ce20b586ec3d8df81c2
pvenableh <peter@huestudios.com>
2023-06-07 20:18:56 -0400

    Update Header.vue

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -7,7 +7,7 @@
       justify-start sm:justify-center px-4 md:px-6 header"
   >
   <Logo/>
-  <LayoutNavButton class="lg:pr-6" />
+  <!-- <LayoutNavButton class="lg:pr-6" /> -->
   </div>
 </template>
 <script setup>
