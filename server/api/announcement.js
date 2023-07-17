109ce10789b407e16a74b8b1b736b9bc9d101efa
pvenableh <peter@huestudios.com>
2023-07-17 13:26:02 -0400

    Updated nitro settings.

---
+++ server/api/announcement.js
@@ -0,0 +1,61 @@
+import sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey('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');
+
+export default defineEventHandler(async (event) => {
+    const body = await readBody(event)
+    const recipients = body.data.recipients
+    const messages = []
+    recipients.forEach(element => {
+        if (element.people_id.email && element.people_id.unit.length > 0) {
+            messages.push(
+                {
+                    personalizations: [{
+                        to: [{
+                            email: element.people_id.email,
+                        }],
+                        bcc: [{
+                            email: 'huestudios.com@gmail.com',
+                        }]
+                    }],
+                    from: {
+                        email: 'mail@1033lenox.com',
+                        name: '1033 Lenox'
+                    },
+                    template_id: 'd-035e7712976d45aaa5143d8a1042aee7',
+                    replyTo: {
+                        email: 'lenoxplazaboard@gmail.com',
+                        name: '1033 Lenox'
+                    },
+                    subject: 'Attention ' + element.people_id.first_name + ': ' + body.data.data.title,
+                    content: [{
+                        type: 'text/html',
+                        value: '&nbsp;'
+                    }],
+                    dynamicTemplateData: {
+                        first_name: element.people_id.first_name,
+                        unit: element.people_id.unit[0].units_id.number,
+                        title: body.data.data.title,
+                        subtitle: body.data.data.subtitle,
+                        urgent: body.data.data.urgent,
+                        content: body.data.data.content,
+                        url: body.data.data.url,
+                        closing: body.data.data.closing,
+                    },
+                    categories: [
+                        '1033 Lenox', 'announcements'
+                    ],
+                }
+            )
+        }
+    })
+ 
+
+    sgMail.send(messages).then(() => {}, error => {
+        console.error(error)
+        if (error.response) {
+            console.error(error.response.body)
+            const error = error.response.body
+        }
+  })
+})
