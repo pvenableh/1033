5064cf38c57ba81d1228e58e4ab1a25d7d88ec76
pvenableh <peter@huestudios.com>
2023-07-17 13:54:10 -0400

    Create test.js

---
+++ server/api/email/test.js
@@ -0,0 +1,56 @@
+import sgMail from '@sendgrid/mail'
+
+sgMail.setApiKey(
+  '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'
+)
+
+export default defineEventHandler(async (event) => {
+  const message = {
+    personalizations: [
+      {
+        to: [
+          {
+            email: 'contact@huestudios.com',
+          },
+        ],
+
+        bcc: [
+          {
+            email: 'huestudios.com@gmail.com',
+          },
+        ],
+      },
+    ],
+    from: {
+      email: 'mail@1033lenox.com',
+      name: '1033 Lenox',
+    },
+    template_id: 'd-ae769660ef18488487809467997242f4',
+    replyTo: {
+      email: 'lenoxplazaboard@gmail.com',
+      name: 'Lenox Plaza Board',
+    },
+    subject: 'Password Reset for 1033lenox.com',
+    content: [
+      {
+        type: 'text/html',
+        value: '&nbsp;',
+      },
+    ],
+    dynamicTemplateData: {
+      user_email: 'contact@huestudios.com',
+      email: 'dslfkjiiojewr980sdu09sda09',
+    },
+    categories: ['1033 Lenox'],
+  }
+  sgMail.send(message).then(
+    () => { console.log('Mail sent successfully') },
+    (error) => {
+      console.error(error)
+      if (error.response) {
+        console.error(error.response.body)
+        const error = error.response.body
+      }
+    }
+  )
+})
