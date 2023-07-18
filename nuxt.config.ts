a0d6ba8acd062f5fbe6516dbbab3f6fc4ae32a9e
pvenableh <peter@huestudios.com>
2023-07-18 10:52:17 -0400

    APIt testing.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,7 +52,6 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
     public: {
       directusUrl: 'https://admin.1033lenox.com',
       plausible: {
