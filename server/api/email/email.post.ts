fd02edf125d05e172296265da6b7b76b20963c29
pvenableh <peter@huestudios.com>
2023-07-18 12:20:02 -0400

    Update email.post.ts

--- server/api/email/email.post.ts
+++ server/api/email/email.post.ts
@@ -2,7 +2,55 @@
 export default defineEventHandler(async (event) => {
     const config = useRuntimeConfig();
     const body = await readBody(event)
-    console.log(body)
+
+    const recipients = body.data.recipients
+    const messages = []
+    if(recipients.length > 0) {
+      recipients.forEach(element => {
+        if (element.people_id.email && element.people_id.unit.length > 0) {
+            messages.push(
+              {
+                personalizations: [{
+                    to: [{
+                        email: element.people_id.email,
+                    }],
+                    bcc: [{
+                        email: 'huestudios.com@gmail.com',
+                    }]
+                }],
+                from: {
+                    email: 'mail@1033lenox.com',
+                    name: '1033 Lenox'
+                },
+                template_id: 'd-035e7712976d45aaa5143d8a1042aee7',
+                replyTo: {
+                    email: 'lenoxplazaboard@gmail.com',
+                    name: '1033 Lenox'
+                },
+                subject: 'Attention ' + element.people_id.first_name + ': ' + body.data.data.title,
+                content: [{
+                    type: 'text/html',
+                    value: '&nbsp;'
+                }],
+                dynamicTemplateData: {
+                    first_name: element.people_id.first_name,
+                    unit: element.people_id.unit[0].units_id.number,
+                    title: body.data.data.title,
+                    subtitle: body.data.data.subtitle,
+                    urgent: body.data.data.urgent,
+                    content: body.data.data.content,
+                    url: body.data.data.url,
+                    closing: body.data.data.closing,
+                },
+                categories: [
+                    '1033 Lenox', 'announcements'
+                ],
+            }
+          )
+        }
+      })
+    }
+    console.log(messages)
     const message = {
         personalizations: [
           {
@@ -42,18 +90,18 @@ export default defineEventHandler(async (event) => {
         categories: ['1033 Lenox'],
       }
  
-    const sgRequest = await $fetch("https://api.sendgrid.com/v3/mail/send",
-        {
-            method: "POST",
-            headers: {
-                Authorization: "Bearer " + config.SENDGRID_API_KEY,
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            },
-        body: JSON.stringify(message)
-    }).catch((error) => {
-        console.log(error)
-        return error;
-    })
+      const sgRequest = await $fetch("https://api.sendgrid.com/v3/mail/send",
+          {
+              method: "POST",
+              headers: {
+                  Authorization: "Bearer " + config.SENDGRID_API_KEY,
+                  "Content-Type": "application/json",
+                  Accept: "application/json"
+              },
+          body: JSON.stringify(message)
+      }).catch((error) => {
+          console.log(error)
+          return error;
+      })
     return sgRequest;
 })
