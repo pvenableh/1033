0dbf10d0c2177373a7a700cf2e5efe84c1e2fbe3
pvenableh <peter@huestudios.com>
2023-07-18 11:35:26 -0400

    Update sendgrid.post.ts

--- server/api/sendgrid.post.ts
+++ server/api/sendgrid.post.ts
@@ -1,59 +1,65 @@
-import sgMail from '@sendgrid/mail'
 
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig()
-  const body = await readBody(event)
-  console.log(body)
-    sgMail.setApiKey(config.SENDGRID_API_KEY)
-  const message = {
-    personalizations: [
-      {
-        to: [
-          {
-            email: 'contact@huestudios.com',
-          },
-        ],
-
-        bcc: [
-          {
-            email: 'huestudios.com@gmail.com',
-          },
-        ],
-      },
-    ],
-    from: {
-      email: 'mail@1033lenox.com',
-      name: '1033 Lenox',
-    },
-    template_id: 'd-ae769660ef18488487809467997242f4',
-    replyTo: {
-      email: 'lenoxplazaboard@gmail.com',
-      name: 'Lenox Plaza Board',
-    },
-    subject: 'Password Reset for 1033lenox.com',
-    content: [
-      {
-        type: 'text/html',
-        value: '&nbsp;',
-      },
-    ],
-    dynamicTemplateData: {
-      user_email: 'contact@huestudios.com',
-      email: 'dslfkjiiojewr980sdu09sda09',
-    },
-    categories: ['1033 Lenox'],
-  }
-  sgMail.send(message).then(
-    () => {
-      console.log('Mail sent successfully')
-    },
-    (error) => {
-      console.error(error)
-      if (error.response) {
-        console.error(error.response.body)
-        console.log(error)
-        // const error = error.response.body
-      }
-    }
-  )
+    const config = useRuntimeConfig();
+    
+    const body = await readBody(event)
+    const recipients = body.data.recipients
+    const messages = []
+    recipients.forEach(element => {
+        if (element.people_id.email && element.people_id.unit.length > 0) {
+            messages.push(
+                {
+                    personalizations: [{
+                        to: [{
+                            email: element.people_id.email,
+                        }],
+                        bcc: [{
+                            email: 'huestudios.com@gmail.com',
+                        }]
+                    }],
+                    from: {
+                        email: 'mail@1033lenox.com',
+                        name: '1033 Lenox'
+                    },
+                    template_id: 'd-035e7712976d45aaa5143d8a1042aee7',
+                    replyTo: {
+                        email: 'lenoxplazaboard@gmail.com',
+                        name: '1033 Lenox'
+                    },
+                    subject: 'Attention ' + element.people_id.first_name + ': ' + body.data.data.title,
+                    content: [{
+                        type: 'text/html',
+                        value: '&nbsp;'
+                    }],
+                    dynamicTemplateData: {
+                        first_name: element.people_id.first_name,
+                        unit: element.people_id.unit[0].units_id.number,
+                        title: body.data.data.title,
+                        subtitle: body.data.data.subtitle,
+                        urgent: body.data.data.urgent,
+                        content: body.data.data.content,
+                        url: body.data.data.url,
+                        closing: body.data.data.closing,
+                    },
+                    categories: [
+                        '1033 Lenox', 'announcements'
+                    ],
+                }
+            )
+        }
+    })
+ 
+    const sgRequest = await $fetch("https://api.sendgrid.com/v3/mail/send",
+        {
+            method: "POST",
+            headers: {
+                Authorization: "Bearer " + config.SENDGRID_API_KEY,
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+        body: JSON.stringify(messages)
+    }).catch((error) => {
+        return error;
+    })
+    return sgRequest;
 })
