3c8a8ec86c7b861cbd77c791196d771246fca1fb
pvenableh <peter@huestudios.com>
2023-11-22 14:52:30 -0500

    Update Tenant.vue

--- components/Insights/Tenant.vue
+++ components/Insights/Tenant.vue
@@ -30,10 +30,19 @@ const activeLease = computed(() => {
 			<span v-if="tenant.phone" class="opacity-50">Phone:</span>
 			{{ tenant.phone }}
 		</p>
-		<p v-if="tenant.leases && activeLease" class="tracking-wide uppercase text-sm"><span class="opacity-50">Lease:</span> Expires on {{ getFriendlyDateTwo(activeLease.finish) }} </p>
+		<p v-if="tenant.leases && activeLease" class="tracking-wide uppercase text-sm">
+			<span class="opacity-50">Lease:</span>
+			Expires on {{ getFriendlyDateTwo(activeLease.finish) }}
+		</p>
 		<p v-else class="tracking wide text-red uppercase font-bold text-sm">No Lease!</p>
-		<a v-if="tenant.leases && activeLease" :href="'https://admin.1033lenox.com/assets/' + activeLease.file" class="mt-2 btn">View Lease Document</a>
-
+		<a
+			v-if="tenant.leases && activeLease"
+			:href="'https://admin.1033lenox.com/assets/' + activeLease.file"
+			class="mt-2 btn"
+			target="_blank"
+		>
+			View Lease Document
+		</a>
 	</div>
 </template>
 <style></style>
