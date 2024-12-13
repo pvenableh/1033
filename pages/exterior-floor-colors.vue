d36bb22e7c2d122fb23c207c7d0e4e7e9ab99daa
Peter Hoffman <peter@huestudios.com>
2024-12-13 17:45:10 -0500

    Updated link.

--- pages/exterior-floor-colors.vue
+++ pages/exterior-floor-colors.vue
@@ -183,7 +183,6 @@ const mailtoLink = computed(() => {
 					:to="mailtoLink"
 					target="_blank"
 					rel="noopener noreferrer"
-					@click="sendConfetti()"
 					class="rounded-sm border uppercase tracking-wide border-gray-500 px-4 py-2 inline-block bg-cover bg-no-repeat bg-center text-white bg-slate-700"
 					:style="'background-image: url(https://admin.1033lenox.com/assets/' + selectedItem.image + '?key=medium-png)'"
 				>
