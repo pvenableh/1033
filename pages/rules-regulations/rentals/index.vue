ce07435ff1b6935c2fc642db6c4669c9e43e971f
Peter Hoffman <peter@huestudios.com>
2025-01-24 15:35:57 -0500

    Added rentals page.

---
+++ pages/rules-regulations/rentals/index.vue
@@ -0,0 +1,85 @@
+<template>
+	<div class="relative w-full min-h-screen flex items-center justify-start flex-col">
+		<h1 class="w-full uppercase tracking-wider mt-6 mb-6 pb-2 text-center border-b max-w-2xl">
+			Rental Rules and Procedures
+		</h1>
+
+		<div class="w-full max-w-2xl px-6 rule">
+			<div v-html="page[0].description"></div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+const { readItems } = useDirectusItems();
+
+const page = await readItems('rules', {
+	fields: ['*'],
+	limit: 1,
+	filter: {
+		url: {
+			_eq: 'rentals',
+		},
+	},
+});
+</script>
+<style>
+.rule {
+	@media (min-width: theme('screens.lg')) {
+	}
+	strong {
+		@apply font-bold;
+	}
+
+	h1 {
+		font-size: 24px;
+		letter-spacing: 0.05em;
+		@apply pt-10 mt-10 uppercase font-bold;
+	}
+
+	h2 {
+		font-size: 20px;
+		text-decoration: none !important;
+		letter-spacing: 0.05em;
+		@apply mt-6 uppercase font-bold;
+	}
+
+	h3 {
+		font-size: 16px;
+		text-decoration: none !important;
+		letter-spacing: 0.05em;
+		@apply mt-6 uppercase font-bold;
+	}
+
+	p {
+		line-height: 1.4em;
+		@apply my-3;
+	}
+
+	ol,
+	ul {
+		@apply ml-8;
+		list-style-type: disc;
+
+		li {
+			line-height: 1.4em;
+			@apply pl-2 my-3;
+		}
+	}
+
+	ol {
+		list-style-type: decimal;
+	}
+
+	a {
+		text-decoration: underline;
+		color: var(--blue);
+	}
+}
+
+h1 {
+	@media (min-width: theme('screens.md')) {
+		max-width: 400px;
+	}
+}
+</style>
